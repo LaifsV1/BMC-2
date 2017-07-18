de39b4d3ad0f30870d7caa2748cbb4271866aa10
Yu-Yang Lin <yyl30@thunderbird-pc.eecs.qmul.ac.uk>
2017-07-18 20:27:57 +0100

    added nil/fail propagation to reduce formula size

--- src/Translation.ml
+++ src/Translation.ml
@@ -45,94 +45,99 @@ let rec subs (m : term) (t : term) (y : _var) =
 (*******************)
 (* BMC Translation *)
 (*******************)
-let f a b p =
-  ((a === tfail) ==> (b === tfail))
-  &&& ((a === tnil) ==> (b === tnil))
-  &&& (((a === tfail) ||| (a === tnil)) ||| p)
+let f a b p q =
+  if q
+  then ((a === tfail) ==> (b === tfail))
+       &&& ((a === tnil) ==> (b === tnil))
+       &&& (((a === tfail) ||| (a === tnil)) ||| p)
+  else p
 
 let rec bmc_translation
           (m : term) (r : repo) (c : counter) (d : counter)
           (phi : proposition) (k : nat)
-        :(_ret * proposition * repo * counter * counter) =
+        :(_ret * proposition * repo * counter * counter * bool) =
   let ret = fresh_ret () in
   match k with
-  | Nil -> (ret,(ret===tnil) &&& phi,r,c,d)
+  | Nil -> (ret,(ret===tnil) &&& phi,r,c,d,true)
   | Suc(k') ->
      (match m with
-      | Fail | Skip | Int _ | Method _ ->
-         let s = (string_of_term m) in (ret,(ret===s) &&& phi,r,c,d)
-      | Var(x,t) -> (ret,(ret===x) &&& phi,r,c,d)
+      (* base cases *)
+      | Fail -> (ret,(ret===(string_of_term m)) &&& phi,r,c,d,true)
+      | Skip | Int _ | Method _ ->
+         let s = (string_of_term m) in (ret,(ret===s) &&& phi,r,c,d,false)
+      | Var(x,t) -> (ret,(ret===x) &&& phi,r,c,d,false)
       | Deref aref ->
-         let d_r = ref_get d aref in (ret,(ret===d_r) &&& phi,r,c,d)
+         let d_r = ref_get d aref in (ret,(ret===d_r) &&& phi,r,c,d,false)
       | Lambda(x,t,tp') ->
          let new_meth = fresh_m () in
          let r' = repo_update r new_meth (x,t,tp') in
-         (ret,(ret===new_meth) &&& phi,r',c,d)
+         (ret,(ret===new_meth) &&& phi,r',c,d,false)
+      (* inductive cases *)
       | Left t ->
-         let (ret1,phi1,r1,c1,d1) = bmc_translation t r c d phi k in
-         (ret,(f ret1 ret (ret===(sprintf "(Left %s)" ret1))) &&& phi1,r1,c1,d1)
+         let (ret1,phi1,r1,c1,d1,q1) = bmc_translation t r c d phi k in
+         (ret,(f ret1 ret (ret===(sprintf "(Left %s)" ret1)) q1) &&& phi1,r1,c1,d1,q1)
       | Right t ->
-         let (ret1,phi1,r1,c1,d1) = bmc_translation t r c d phi k in
-         (ret,(f ret1 ret (ret===(sprintf "(Right %s)" ret1))) &&& phi1,r1,c1,d1)
+         let (ret1,phi1,r1,c1,d1,q1) = bmc_translation t r c d phi k in
+         (ret,(f ret1 ret (ret===(sprintf "(Right %s)" ret1)) q1) &&& phi1,r1,c1,d1,q1)
       | Assign(aref,t) ->
-         let (ret1,phi1,r1,c1,d1) = bmc_translation t r c d phi k in
+         let (ret1,phi1,r1,c1,d1,q1) = bmc_translation t r c d phi k in
          let c1' = c_update c aref in
          let d1' = d_update d aref (cd_get c1' aref) in
-         (ret,(f ret1 ret (ret===tskip)) &&& phi1,r1,c1',d1')
+         (ret,(f ret1 ret (ret===tskip) q1) &&& phi1,r1,c1',d1',q1)
       | Pair(t1,t2) ->
-         let (ret1,phi1,r1,c1,d1) = bmc_translation t1 r c d phi k in
-         let (ret2,phi2,r2,c2,d2) = bmc_translation t2 r1 c1 d1 phi1 k in
-         (ret,(f ret1 ret (f ret2 ret (ret===(sprintf "(%s,%s)" ret1 ret2))))
-              &&& phi2,r2,c2,d2)
+         let (ret1,phi1,r1,c1,d1,q1) = bmc_translation t1 r c d phi k in
+         let (ret2,phi2,r2,c2,d2,q2) = bmc_translation t2 r1 c1 d1 phi1 k in
+         (ret,(f ret1 ret (f ret2 ret (ret===(sprintf "(%s,%s)" ret1 ret2)) q2) q1)
+              &&& phi2,r2,c2,d2,q1||q2)
       | BinOp(op,t1,t2) ->
-         let (ret1,phi1,r1,c1,d1) = bmc_translation t1 r c d phi k in
-         let (ret2,phi2,r2,c2,d2) = bmc_translation t2 r1 c1 d1 phi1 k in
-         (ret,(f ret1 ret (f ret2 ret (ret===(sprintf "(%s %s %s)" ret1 op ret2))))
-              &&& phi2,r2,c2,d2)
+         let (ret1,phi1,r1,c1,d1,q1) = bmc_translation t1 r c d phi k in
+         let (ret2,phi2,r2,c2,d2,q2) = bmc_translation t2 r1 c1 d1 phi1 k in
+         (ret,(f ret1 ret (f ret2 ret (ret===(sprintf "(%s %s %s)" ret1 op ret2)) q2) q1)
+              &&& phi2,r2,c2,d2,q1||q2)
       | Let((x,tp),t1,t2) ->
-         let (ret1,phi1,r1,c1,d1) = bmc_translation t1 r c d phi k in
-         let (ret2,phi2,r2,c2,d2) = bmc_translation (subs t2 (Var (ret1,tp)) (x,tp))
-                                                    r1 c1 d1 phi1 k in
-         (ret,(f ret1 ret (f ret2 ret (ret===ret2))) &&& phi2,r2,c2,d2)
+         let (ret1,phi1,r1,c1,d1,q1) = bmc_translation t1 r c d phi k in
+         let (ret2,phi2,r2,c2,d2,q2) = bmc_translation (subs t2 (Var (ret1,tp)) (x,tp))
+                                                       r1 c1 d1 phi1 k in
+         (ret,(f ret1 ret (f ret2 ret (ret===ret2) q2) q1) &&& phi2,r2,c2,d2,q1||q2)
       | ApplyM(m,t) ->
-         let (ret1,phi1,r1,c1,d1) = bmc_translation t r c d phi k in
+         let (ret1,phi1,r1,c1,d1,q1) = bmc_translation t r c d phi k in
          let (x,n,tp) = repo_get r m in
-         let (ret2,phi2,r2,c2,d2) = bmc_translation (subs n (Var (ret1,snd x)) x)
-                                                    r1 c1 d1 phi1 k' in
-         (ret,(f ret1 ret (f ret2 ret (ret===ret2))) &&& phi2,r2,c2,d2)
+         let (ret2,phi2,r2,c2,d2,q2) = bmc_translation (subs n (Var (ret1,snd x)) x)
+                                                       r1 c1 d1 phi1 k' in
+         (ret,(f ret1 ret (f ret2 ret (ret===ret2) q2) q1) &&& phi2,r2,c2,d2,q1||q2)
       | If(tb,t1,t0) ->
-         let (retb,phib,rb,cb,db) = bmc_translation tb r c d phi k in
-         let (ret0,phi0,r0,c0,d0) = bmc_translation t0 rb cb db phib k in
-         let (ret1,phi1,r1,c1,d1) = bmc_translation t1 r0 c0 d0 phi0 k in
+         let (retb,phib,rb,cb,db,qb) = bmc_translation tb r c d phi k in
+         let (ret0,phi0,r0,c0,d0,q0) = bmc_translation t0 rb cb db phib k in
+         let (ret1,phi1,r1,c1,d1,q1) = bmc_translation t1 r0 c0 d0 phi0 k in
          let c' = c_update_all c1 in
-         let pi0 = f ret0 ret ((retb === "0") ==> ((ret===ret0) &&& (c_wedge c' d0))) in
-         let pi1 = f ret1 ret ((retb =/= "0") ==> ((ret===ret1) &&& (c_wedge c' d1))) in
-         (ret,(f retb ret (pi0 &&& pi1)) &&& phi1,r1,c',c')
+         let pi0 = f ret0 ret ((retb === "0") ==> ((ret===ret0) &&& (c_wedge c' d0))) q0 in
+         let pi1 = f ret1 ret ((retb =/= "0") ==> ((ret===ret1) &&& (c_wedge c' d1))) q1 in
+         (ret,(f retb ret (pi0 &&& pi1) qb) &&& phi1,r1,c',c',qb||q0||q1)
       | ApplyX((x,tp),t) ->
-         let (ret0,phi0,r0,c0,d0) = bmc_translation t r c d phi k in
+         let (ret0,phi0,r0,c0,d0,q0) = bmc_translation t r c d phi k in
          let r_tp = get_methods r0 tp in
          let xs =
            List.fold_left
              (fun ys (mi,xi,ti) ->
                match ys with
                | [] -> failwith "variable type does not match any existing method (1)"
-               | ((mi_,reti_,phii_,ri_,ci_,di_)::ys') ->
-                  let (reti,phii,ri,ci,di) = bmc_translation (subs ti (Var (ret0,tp)) (x,tp))
-                                                             ri_ ci_ d0 phii_ k' in
+               | ((mi_,reti_,phii_,ri_,ci_,di_,qi_)::ys') ->
+                  let (reti,phii,ri,ci,di,qi) = bmc_translation (subs ti (Var (ret0,tp)) (x,tp))
+                                                                ri_ ci_ d0 phii_ k' in
                   if mi_ = "_not_a_method_"
-                  then [(mi,reti,phii,ri,ci,di)]
-                  else (mi,reti,phii,ri,ci,di)::ys)
-             ["_not_a_method_",ret0,phi0,r0,c0,d0] r_tp in
+                  then [(mi,reti,phii,ri,ci,di,qi||qi_)]
+                  else (mi,reti,phii,ri,ci,di,qi||qi_)::ys)
+             ["_not_a_method_",ret0,phi0,r0,c0,d0,q0] r_tp in
          match xs with
          | [] -> failwith "variable type does not match any existing method (2)"
-         | ((mn,retn,phin,rn,cn,dn)::xs) ->
+         | ((mn,retn,phin,rn,cn,dn,qn)::xs) ->
             let cn' = c_update_all cn in
             let pi =
               List.fold_left
-                (fun acc (mi,reti,_,_,_,di) ->
-                  (x===mi) ==> ((f reti ret (ret===reti)) &&& c_wedge cn' di))
-                True ((mn,retn,phin,rn,cn,dn)::xs) in
-            (ret,(f ret0 ret pi) &&& phin,rn,cn',cn'))
+                (fun acc (mi,reti,_,_,_,di,qi) ->
+                  (x===mi) ==> ((f reti ret (ret===reti) qi) &&& c_wedge cn' di))
+                True ((mn,retn,phin,rn,cn,dn,qn)::xs) in
+            (ret,(f ret0 ret pi q0) &&& phin,rn,cn',cn',qn))
 
 (****************)
 (*** RUN TEST ***)
@@ -144,6 +149,6 @@ let result = bmc_translation (ApplyM("fact",Int(3)))
                              (empty_counter)
                              (empty_counter) True (nat_of_int 1)
 
-let _ = let (ret,phi,repo,c_counter,d_counter) = result in
+let _ = let (ret,phi,repo,c_counter,d_counter,q) = result in
         printf "Formula:\n %s\n" (string_of_proposition (phi));
         exit 0
