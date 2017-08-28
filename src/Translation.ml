ee53602d9e5917aeceec4a09d521c0195a4851c2
Yu-Yang Lin <yuyanglinhou@hotmail.com>
2017-08-28 21:54:54 +0100

    Fixed ret = fail/nil issue, Made formulas smaller.
    - Added (ret != fail) and (ret != nil) when returning a value. Still
    need to add (fail != nil) for every possible type.
    - Add 4-valued logic to propagate fail/nil up to the rest of the tree.

--- src/Translation.ml
+++ src/Translation.ml
@@ -58,11 +58,34 @@ let rec subs (m : term) (t : _var) (y : _var) =
 (*******************)
 (* BMC Translation *)
 (*******************)
+
+(* error propagation type *)
+type e_prop = Fail | Nil | Both | Val
+let (+++) (a : e_prop) (b : e_prop) =
+  match a,b with
+  | Nil,Fail -> Both
+  | Fail,Nil -> Both
+  | Both,_ -> Both
+  | _,Both -> Both
+  | Nil,_ -> Nil
+  | _,Nil -> Nil
+  | Fail,_ -> Fail
+  | _,Fail -> Fail
+  | Val,Val -> Val
+
 (* fail/nil-guards function*)
-let f (a,tpa : _ret) (b,tpb : _ret) (p : proposition) (q : bool) (acc : _decl) :(proposition * _decl) =
-  let pred faila failb nila nilb = ((a === faila) ==> (b === failb))
-                                   &&& ((a === nila) ==> (b === nilb))
-                                   &&& (((a === faila) ||| (a === nila)) ||| p) in
+let f (a,tpa : _ret) (b,tpb : _ret) (p : proposition) (q : e_prop) (acc : _decl) :(proposition * _decl) =
+  let pred faila failb nila nilb =
+    match q with
+    | Both -> ((a === faila) ==> (b === failb))
+              &&& ((a === nila) ==> (b === nilb))
+              &&& (((a === faila) ||| (a === nila)) ||| p)
+    | Fail -> ((a === faila) ==> (b === failb))
+              &&& ((a === faila) ||| p)
+    | Nil -> ((a === nila) ==> (b === nilb))
+             &&& ((a === nila) ||| p)
+    | Val -> p
+  in
   let select_check (tp : tp) (acc : _decl) :(string * string * _decl) =
     match tp with
       | Unit         -> tfail_u,tnil_u,acc
@@ -75,48 +98,62 @@ let f (a,tpa : _ret) (b,tpb : _ret) (p : proposition) (q : bool) (acc : _decl) :
                         else a,b,acc in
   let faila,nila,decl1 = select_check tpa acc in
   let failb,nilb,decl2 = select_check tpb decl1 in
-  if q then pred faila failb nila nilb , decl2 else p , decl2
+  pred faila failb nila nilb , decl2
 
 (* translation *)
 let rec bmc_translation
           (m : term) (r : repo) (c : counter) (d : counter)
           (phi : proposition) (k : nat) (etype : tp) (tps : (_name * z3_tp) list)
-        :(_ret * proposition * repo * counter * counter * bool * (_name * z3_tp) list) =
+        :(_ret * proposition * repo * counter * counter * e_prop * (_name * z3_tp) list) =
   let ret = fresh_ret () in
   let ret_tp = ret,etype in
   let new_tps = (ret,z3_of_tp etype)::tps in (* (ret1,type1)::(ret2,type2)::tps *)
   match k with
   | Nil ->
      (match etype with
-      | Unit    -> (ret_tp,(ret===tnil_u) &&& phi,r,c,d,true,new_tps)
-      | Integer -> (ret_tp,(ret===tnil_i) &&& phi,r,c,d,true,new_tps)
-      | Arrow _ -> (ret_tp,(ret===tnil_m) &&& phi,r,c,d,true,new_tps)
+      | Unit    -> (ret_tp,(ret===tnil_u) &&& phi,r,c,d,Nil,new_tps)
+      | Integer -> (ret_tp,(ret===tnil_i) &&& phi,r,c,d,Nil,new_tps)
+      | Arrow _ -> (ret_tp,(ret===tnil_m) &&& phi,r,c,d,Nil,new_tps)
       | Product (tp1,tp2) ->
          let n,is_new = get_type_number etype in
          let new_tps' = if is_new then (tnil_n n,z3_of_tp etype)::new_tps else new_tps in
-         (ret_tp,(ret===(tnil_n n)) &&& phi,r,c,d,true,new_tps'))
+         (ret_tp,(ret===(tnil_n n)) &&& phi,r,c,d,Nil,new_tps'))
   | Suc(k') ->
      (match m with
       (* base cases *)
       | Fail ->
          (match etype with
-          | Unit    -> (ret_tp,(ret===tfail_u) &&& phi,r,c,d,true,new_tps)
-          | Integer -> (ret_tp,(ret===tfail_i) &&& phi,r,c,d,true,new_tps)
-          | Arrow _ -> (ret_tp,(ret===tfail_m) &&& phi,r,c,d,true,new_tps)
+          | Unit    -> (ret_tp,(ret===tfail_u) &&& phi,r,c,d,Fail,new_tps)
+          | Integer -> (ret_tp,(ret===tfail_i) &&& phi,r,c,d,Fail,new_tps)
+          | Arrow _ -> (ret_tp,(ret===tfail_m) &&& phi,r,c,d,Fail,new_tps)
           | Product (tp1,tp2) ->
              let n,is_new = get_type_number etype in
              let new_tps' = if is_new then (tfail_n n,z3_of_tp etype)::new_tps else new_tps in
-             (ret_tp,(ret===(tfail_n n)) &&& phi,r,c,d,true,new_tps'))
-      | Skip -> (ret_tp,(ret===tskip) &&& phi,r,c,d,false,new_tps)
-      | Int i -> (ret_tp,(ret===(string_of_int i)) &&& phi,r,c,d,false,new_tps)
-      | Method m -> (ret_tp,(ret===(z3_method m)) &&& phi,r,c,d,false,new_tps)
-      | Var(x,t) -> (ret_tp,(ret===x) &&& phi,r,c,d,false,new_tps)
+             (ret_tp,(ret===(tfail_n n)) &&& phi,r,c,d,Fail,new_tps'))
+      | Skip -> (ret_tp,(ret===tskip) &&& (ret=/=tfail_u) &&& (ret=/=tnil_u) &&& phi,r,c,d,Val,new_tps)
+      | Int i -> (ret_tp,(ret===(string_of_int i)) &&& (ret=/=tfail_i) &&& (ret=/=tnil_i) &&& phi,r,c,d,Val,new_tps)
+      | Method m -> (ret_tp,(ret===(z3_method m)) &&& (ret=/=tfail_m) &&& (ret=/=tnil_m) &&& phi,r,c,d,Val,new_tps)
+      | Var(x,t) ->
+         (match etype with
+          | Unit    -> (ret_tp,(ret===x) &&& (ret=/=tfail_u) &&& (ret=/=tnil_u) &&& phi,r,c,d,Val,new_tps)
+          | Integer -> (ret_tp,(ret===x) &&& (ret=/=tfail_i) &&& (ret=/=tnil_i) &&& phi,r,c,d,Val,new_tps)
+          | Arrow _ -> (ret_tp,(ret===x) &&& (ret=/=tfail_m) &&& (ret=/=tnil_m) &&& phi,r,c,d,Val,new_tps)
+          | Product (tp1,tp2) ->
+             let n,is_new = get_type_number etype in
+             let new_tps' = if is_new then (tfail_n n,z3_of_tp etype)::new_tps else new_tps in
+             (ret_tp,(ret===x) &&& (ret=/=(tfail_n n)) &&& (ret=/=(tnil_n n)) &&& phi,r,c,d,Val,new_tps'))
       | Deref aref ->
-         let d_r = ref_get d aref in (ret_tp,(ret===d_r) &&& phi,r,c,d,false,new_tps)
+         let d_r = ref_get d aref in
+         (match etype with
+          | Unit    -> failwith "you shouldn't be able to store Unit in refs"
+          | Integer -> (ret_tp,(ret===d_r) &&& (ret=/=tfail_i) &&& (ret=/=tnil_i) &&& phi,r,c,d,Val,new_tps)
+          | Arrow _ -> (ret_tp,(ret===d_r) &&& (ret=/=tfail_m) &&& (ret=/=tnil_m) &&& phi,r,c,d,Val,new_tps)
+          | Product (tp1,tp2) -> failwith "you shouldn't be able to store products in refs")
       | Lambda(x,t,tp') ->
          let new_meth = fresh_m () in
          let r' = repo_update r new_meth (x,t,tp') in
-         (ret_tp,(ret===(z3_method new_meth)) &&& phi,r',c,d,false,(new_meth,Meth)::new_tps)
+         (ret_tp,(ret===(z3_method new_meth)) &&& (ret=/=tfail_m) &&& (ret=/=tnil_m) &&& phi,
+          r',c,d,Val,(new_meth,Meth)::new_tps)
       (* inductive cases *)
       | Left (t,tp) ->
          let (ret1,phi1,r1,c1,d1,q1,tps1) = bmc_translation t r c d phi k tp new_tps in
@@ -139,20 +176,20 @@ let rec bmc_translation
          let (ret2,phi2,r2,c2,d2,q2,tps2) = bmc_translation t2 r1 c1 d1 phi1 k (right_tp etype) tps1 in
          let guard_1,tpsg1 = (f ret2 ret_tp (ret===(z3_pair_maker (fst ret1) (fst ret2))) q2 tps2) in
          let guard_2,tpsg2 = (f ret1 ret_tp guard_1 q1 tpsg1) in
-         (ret_tp,guard_2 &&& phi2,r2,c2,d2,q1||q2,tpsg2)
+         (ret_tp,guard_2 &&& phi2,r2,c2,d2,q1+++q2,tpsg2)
       | BinOp(op,t1,t2) ->
          let (ret1,phi1,r1,c1,d1,q1,tps1) = bmc_translation t1 r c d phi k (etype) new_tps in
          let (ret2,phi2,r2,c2,d2,q2,tps2) = bmc_translation t2 r1 c1 d1 phi1 k (etype) tps1 in
          let guard_1,tpsg1 = (f ret2 ret_tp (ret===(z3_binops (fst ret1) op (fst ret2))) q2 tps2) in
          let guard_2,tpsg2 = (f ret1 ret_tp guard_1 q1 tpsg1) in
-         (ret_tp,guard_2 &&& phi2,r2,c2,d2,q1||q2,tpsg2)
+         (ret_tp,guard_2 &&& phi2,r2,c2,d2,q1+++q2,tpsg2)
       | Let((x,tp),t1,t2) ->
          let (ret1,phi1,r1,c1,d1,q1,tps1) = bmc_translation t1 r c d phi k tp new_tps in
          let (ret2,phi2,r2,c2,d2,q2,tps2) = bmc_translation (subs t2 ret1 (x,tp))
                                                             r1 c1 d1 phi1 k etype tps1 in
          let guard_1,tpsg1 = (f ret2 ret_tp (ret===(fst ret2)) q2 tps2) in
          let guard_2,tpsg2 = (f ret1 ret_tp guard_1 q1 tpsg1) in
-         (ret_tp,guard_2 &&& phi2,r2,c2,d2,q1||q2,tpsg2)
+         (ret_tp,guard_2 &&& phi2,r2,c2,d2,q1+++q2,tpsg2)
       | ApplyM(m,t) ->
          let (x,n,tp) = repo_get r m in
          let (ret1,phi1,r1,c1,d1,q1,tps1) = bmc_translation t r c d phi k (snd x) new_tps in
@@ -160,7 +197,7 @@ let rec bmc_translation
                                                             r1 c1 d1 phi1 k' etype tps1 in
          let guard_1,tpsg1 = (f ret2 ret_tp (ret===(fst ret2)) q2 tps2) in
          let guard_2,tpsg2 = (f ret1 ret_tp guard_1 q1 tpsg1) in
-         (ret_tp,guard_2 &&& phi2,r2,c2,d2,q1||q2,tpsg2)
+         (ret_tp,guard_2 &&& phi2,r2,c2,d2,q1+++q2,tpsg2)
       | If(tb,t1,t0) ->
          let (retb,phib,rb,cb,db,qb,tpsb) = bmc_translation tb r c d phi k Integer new_tps in
          let (ret0,phi0,r0,c0,d0,q0,tps0) = bmc_translation t0 rb cb db phib k etype tpsb in
@@ -171,7 +208,7 @@ let rec bmc_translation
          let pi0 = ((fst retb) === "0") ==> guard_0 in
          let pi1 = ((fst retb) =/= "0") ==> guard_1 in
          let guard_b,tpsgb = (f retb ret_tp (pi0 &&& pi1) qb tpsg1) in
-         (ret_tp,guard_b &&& phi1,r1,c',c',qb||q0||q1,tpsgb)
+         (ret_tp,guard_b &&& phi1,r1,c',c',qb+++q0+++q1,tpsgb)
       | ApplyX((x,tp),t) ->
          (match tp with
           | Arrow(one,two) ->
@@ -186,8 +223,8 @@ let rec bmc_translation
                        let (reti,phii,ri,ci,di,qi,tpsi) = bmc_translation (subs ti ret0 xi)
                                                                           ri_ ci_ d0 phii_ k' etype tpsi_ in
                        if mi_ = "_not_a_method_"
-                       then phii,ri,ci,[(mi,reti,di,qi||qi_)],tpsi
-                       else phii,ri,ci,((mi,reti,di,qi||qi_)::ys),tpsi)
+                       then phii,ri,ci,[(mi,reti,di,qi+++qi_)],tpsi
+                       else phii,ri,ci,((mi,reti,di,qi+++qi_)::ys),tpsi)
                   (phi0,r0,c0,["_not_a_method_",ret0,d0,q0],tps0) r_tp in
               match xs with
               | [] -> failwith "variable type does not match any existing method (2)"
