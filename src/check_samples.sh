91bdc4fb22f2ae57de1ca9d6f1412c0c313b3d72
Yu-Yang Lin <LaifsV1@gmail.com>
2019-05-09 14:41:14 +0100

    updated test script

--- src/check_samples.sh
+++ src/check_samples.sh
@@ -1,17 +1,53 @@
 #!/bin/sh
-d=exp
+run_tests() {
+    for k in $(eval echo {$1..$2})
+    do
+	echo "===========================";
+	echo "k=$k";
+	for p in ${@:3}
+	do
+	    echo "------------------------";
+	    echo $p;
+	    (echo $p ; time timeout $t ./TopLevel.native ../extras/sample_programs/MoCHi_samples/$p.txt $k | z3 -in | grep -v "(error" ; echo "") &>> $d/$k.txt;
+	    echo "------------------------";
+	done
+    done    
+}
+
+run_all_tests() {
+    run_tests 1 8 copy_intro e-fact e-simple hors intro1 intro3 l-zipmap l-zipunzip max mult-e-m1 mult-e-m2 mult-e mult-m1 mult repeat-e r-lock-e sum-e-m1 sum-e sum hrec ack mc91-e mc91;    
+    run_tests 9 11 copy_intro e-fact e-simple hors intro1 intro3 l-zipmap l-zipunzip max mult-e-m1 mult-e-m2 mult-e mult-m1 mult repeat-e r-lock-e sum-e-m1 sum-e sum hrec mc91-e mc91;
+    run_tests 12 13 copy_intro e-fact e-simple hors intro1 intro3 l-zipmap l-zipunzip max mult-e-m1 mult-e-m2 mult-e mult-m1 mult repeat-e r-lock-e sum-e-m1 sum-e sum mc91-e mc91;
+    run_tests 14 20 copy_intro e-fact e-simple hors intro1 intro3 l-zipmap l-zipunzip max mult-e-m1 mult-e-m2 mult-e mult-m1 mult repeat-e r-lock-e sum-e-m1 sum-e sum;
+}
+
+d=exp1
 t=600
 rm -r $d 2> /dev/null
 mkdir -p $d
-for k in {0..15}
-do
-    echo "===========================";
-    echo "k=$k";
-    for p in ack copy_intro e-fact e-simple hors hrec intro1 intro3 l-zipmap l-zipunzip max mc91-e mc91 mult-e-m1 mult-e-m2 mult-e mult-m1 mult repeat-e r-lock-e sum-e-m1 sum-e sum
-    do
-	echo "------------------------";
-	echo $p;
-	(echo $p ; time timeout $t ./TopLevel.native ../extras/sample_programs/MoCHi_samples/$p.txt $k | z3 -in | grep -v "(error" ; echo "") &>> $d/$k.txt;
-	echo "------------------------";
-    done
-done
+
+run_all_tests
+
+d=exp2
+rm -r $d 2> /dev/null
+mkdir -p $d
+
+run_all_tests
+
+d=exp3
+rm -r $d 2> /dev/null
+mkdir -p $d
+
+run_all_tests
+
+d=exp4
+rm -r $d 2> /dev/null
+mkdir -p $d
+
+run_all_tests
+
+d=exp5
+rm -r $d 2> /dev/null
+mkdir -p $d
+
+run_all_tests
